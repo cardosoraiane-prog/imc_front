63a34fd2f088651ff8b7222e2d4ad538e957d02b
SeuNome <SeuEmail>
2026-07-01 12:04:14 -0300

    new file:   cadastraUsuario.html 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+async function cadastrarUsuario(){
+    const dados = {
+        nome : DocumentFragment.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        senha: document.getElementById("senha").value,
+}
+
+
+try {
+    const res = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dados),
+    });
+
+    const resultado = await res.json();
+    if (resultado.token) {
+      localStorage.setItem("token", resultado.token);
+      window.location.href = "index.html";
+    } else {
+      alert("Cadastro inválidi!");
+    }
+  } catch (erro) {
+    alert("Falha na comunnicação com o servidor.");
+  }
+}
